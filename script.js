f67d0c71a3e2778db6218bc00e11c313409af826
Eniola Hannah <heniola395@gmail.com>
2022-09-28 13:45:28 +0100

    made function  erase clear everything in the display screen

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-firstDisplay.value = 0;
-
 const callValue=(value)=>{
     firstDisplay.value += value
 }
@@ -7,7 +5,7 @@ const operation=()=>{
     console.log(eval(firstDisplay.value));
     secondDisplay.value += `${eval(firstDisplay.value)}`
 }
-const clear=()=>{
-    eval(display.value = "")
-    console.log("e dey work")
+const erase=()=>{
+    firstDisplay.value = ""
+    secondDisplay.value = ""
 }
